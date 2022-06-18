9c7ca948b87dac87aaadfe27a2f8b90f67cf46d4
Todsaporn Laosa <togenius1@gmail.com>
2022-06-18 08:38:29 +0700

    Edit auth file

--- util/auth.js
+++ util/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyC1yrBSOGxpWahJk330ZNC2wTG4qCriCDA';
+const API_KEY = '[YOUR GOOGLE FIREBASE API]';
 
 export async function authenticate(mode, email, password) {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
